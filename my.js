3ef67e8b260087d9ec1e3d05af0408bb2c9bb3fb
Pongpolaem <109690023+Pongpolaem@users.noreply.github.com>
2022-11-02 14:03:41 +0700

    Update my.js

--- my.js
+++ my.js
@@ -23,7 +23,7 @@ function addGS(){
                 },
                 redirect: 'follow', // manual, *follow, error
                 //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify({first:"Dave",phone:"098-786-8722",last:"Vlide"}) // body data type must match "Content-Type" header
+                body: JSON.stringify({body:"Dave"}) // body data type must match "Content-Type" header
         });
 
 }
